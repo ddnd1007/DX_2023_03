b0129fec6ce69fe9242c4ffbc1a8a57177bc5852
BSW <129160465+skytoy12@users.noreply.github.com>
2023-05-16 19:51:18 +0900

    DP, INCHANT 과제

---
+++ 2303_WINAPI/Algorithm/Dp_2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// ENCHANT
+// 0강에서부터 시작
+
+// +1
+// +2
+// +3
+
+// n강을 할 때 강화하는 모든 경우의 수
+
+// 예 : 3강을 하고 싶다. -> 4가지
+// 1 + 1 + 1 
+// 1 + 2 
+// 2 + 1
+// 3
+
+// 예 : 4강 -> 7가지
+// 1 + 1 + 1 + 1
+// 1 + 2 + 1
+// 1 + 1 + 2
+// 1 + 3
+// 2 + 1 + 1
+// 2 + 2
+// 3 + 1
+
+
+
+int Enchant(int n, int m, int k)
+{
+	if ()
+}
+
+
+int main()
+{
+
+	return 0;
+}
