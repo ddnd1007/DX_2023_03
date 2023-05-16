879766be381dd38c821d87ef989201390df7a844
윤희준 <ddnd1007@naver.com>
2023-05-16 22:45:38 +0900

    Enchant 숙제

--- 2303_WINAPI/Algorithm/Dp_2.cpp
+++ 2303_WINAPI/Algorithm/Dp_2.cpp
@@ -28,16 +28,56 @@ using namespace std;
 // 2 + 2
 // 3 + 1
 
-
-
-int Enchant(int n, int m, int k)
+vector<vector<int>> dp;
+int  Enchant(int n, int m)
 {
-	if ()
+    
+    if (n == 0)
+       return 1;
+
+    if (n < 0 || m == 0)
+        return 0;
+    
+
+    if (dp[n][m] != -1)
+        return dp[n][m];
+
+    return dp[n][m] = Enchant(n, m - 1) + Enchant(n - m, m);
+
 }
 
-
+void printPermutations(vector<int>& permutation)
+{
+    while (true)
+    {
+        if (std::next_permutation(permutation.begin(), permutation.end()))
+        {
+            for (int value : permutation)
+            {
+                cout << value << " ";
+            }
+            cout << endl;
+        }
+        else
+            break;
+    }
+    return;
+}
 int main()
 {
+    int n = 6; // 강화 횟수
+    int m = 3; // 최대 강화 옵션
+   
+    dp = vector<vector<int>>(n + 1, vector<int>(m + 1, -1));
 
-	return 0;
+    vector<int> options;
+    for (int i = 0; i <= m; i++)
+    {
+        options.push_back(i);
+    }
+     
+    printPermutations(options);
+   
+        
+    return 0;
 }
