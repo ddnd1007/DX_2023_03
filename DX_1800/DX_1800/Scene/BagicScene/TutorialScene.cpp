57a2d769e0adc232a4f9cc9018d26f2d72d2e01d
윤희준 <ddnd1007@naver.com>
2023-05-29 20:00:48 +0900

    이미지 이동,확대,축소,회전

--- DX_1800/DX_1800/Scene/BagicScene/TutorialScene.cpp
+++ DX_1800/DX_1800/Scene/BagicScene/TutorialScene.cpp
@@ -24,33 +24,45 @@ TutorialScene::~TutorialScene()
 
 void TutorialScene::Update()
 {
-	
-
-	if (GetAsyncKeyState('A'))
+	// 키보드 입력 처리
+	if (GetAsyncKeyState('W') & 0x8000) // W 키
+		Move(0.0f, 0.003f, 0.0f); // 위로 이동
+	if (GetAsyncKeyState('S') & 0x8000) // S 키
+		Move(0.0f, -0.003f, 0.0f); // 아래로 이동
+	if (GetAsyncKeyState('A') & 0x8000) // A 키
+		Move(-0.003f, 0.0f, 0.0f); // 왼쪽으로 이동
+	if (GetAsyncKeyState('D') & 0x8000) // D 키
+		Move(0.003f, 0.0f, 0.0f); // 오른쪽으로 이동
+	if (GetAsyncKeyState('Q') & 0x8000) // Q 키
 	{
-		_x -= 0.1f;
+		_angle -= XMConvertToRadians(0.1f); // 반시계 방향으로 회전
+		if (_angle < 0.0f)
+			_angle += XM_2PI;
 	}
-	if (GetAsyncKeyState('D'))
+	if (GetAsyncKeyState('R') & 0x8000) // R 키
 	{
-		_x += 0.1f;
+		_angle += XMConvertToRadians(0.1f); // 시계 방향으로 회전
+		if (_angle >= XM_2PI)
+			_angle -= XM_2PI;
 	}
-	if (GetAsyncKeyState('W'))
-	{
-		_y += 0.1f;
-	}
-	if (GetAsyncKeyState('S'))
-	{
-		_y -= 0.1f;
-	}
+	if (GetAsyncKeyState('F') & 0x8000) // F 키
+		_scale += 0.0001f; // 확대
+	if (GetAsyncKeyState('G') & 0x8000) // G 키
+		_scale -= 0.0001f; // 축소
 
 	// 정점 = (SRT = world * view, porj)
 	//XMMatrixScaling()
 	//XMMatrixRotationZ() 
-
-
+	
+	// 월드 행렬
 	XMMATRIX translate = XMMatrixTranslation(_x, _y, 0);
+	XMMATRIX scaling = XMMatrixScaling(_scale, _scale, 1.0f);
+	XMMATRIX rotation = XMMatrixRotationZ(_angle);
+	
 
-	_world->SetData(translate);
+	XMMATRIX worldMatrix = scaling * rotation * translate;
+
+	_world->SetData(worldMatrix);
 	_world->Update_Resource();
 
 	_quad->Update();
